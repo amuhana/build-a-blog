965229368d95e208863462e46a122f1406bf43d6
amuhana <amin.muhana@gmail.com>
2019-11-08 15:08:24 -0600

    Change main route path

--- main.py
+++ main.py
@@ -3,19 +3,22 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABSE_URI'] = 'mysql+pymysql://build-a-blog:beproductive@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:beproductive@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
-
 class Blog(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	title = db.Column(db.String(120))
-	body = db.Column(db.String(120))
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(120))
 
-@app.route("/blog")
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+@app.route("/")
 def index():
-	return render_template("blog.html")
+	return rediret('/blog')
 
 
 if __name__ == '__main__':
