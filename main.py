cc1dc1166222d2323ca599f6fe2466070dd6422a
amuhana <amin.muhana@gmail.com>
2019-11-08 18:57:19 -0600

    Add newpost route and function handler

--- main.py
+++ main.py
@@ -26,5 +26,40 @@ def display_blog():
 	return render_template("blog.html")
 
 
+@app.route("/newpost")
+def newpost():
+	return render_template("newpost.html", heading="Add a Blog Entry")
+
+
+@app.route('/newpost', methods=['POST'])
+def new_post():
+	""" New post"""
+
+	if request.method == 'POST':
+		blog_title = request.form['blog-title']
+		blog_body = request.form['blog-body']
+		title_error = ""
+		body_error = ""
+		
+		
+
+		if not blog_title:
+			title_error = "Please enter a title"
+		if not blog_body:
+			body_error = "Please enter a blog entry"
+
+
+		if not title_error and not body_error:
+			new_post = Blog(blog_title, blog_body)
+			db.session.add(new_post)
+			db.session.commit()
+			return redirect("/blog")
+		else:
+			return render_template("newpost.html", title="New Entry", title_error=title_error, body_error=body_error, blog_title=blog_title, blog_body=blog_body)
+	return render_template("newpost.html", title="New Entry")
+
+
+
+
 if __name__ == '__main__':
 	app.run()
